cf79ae683a539b74128c76059da5398b11a42946
kapepa <kapepayx@gmail.com>
2023-07-01 23:53:15 +0300

    app: feature/common | unit-test

--- app/src/widgets/LangSwitcher/ui/LangSwitcher.test.tsx
+++ app/src/widgets/LangSwitcher/ui/LangSwitcher.test.tsx
@@ -2,15 +2,6 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import LangSwitcher from "./LangSwitcher";
 
-
-jest.mock("../../../shared/ui/Button/Button", () => ({
-  ...jest.requireActual("../../../shared/ui/Button/Button"),
-  __esModule: true,
-  default: jest.fn(({children, onClick}) => {
-    return <button role="button" onClick={onClick}>{children}</button>
-  })
-}))
-
 jest.mock('react-i18next', () => ({
   useTranslation: () => ({
     t: (str: string): string => str,
@@ -18,10 +9,9 @@ jest.mock('react-i18next', () => ({
   })
 }));
 
-test('renders LangSwitcher', async () => {
-  const {getByRole} = render(<LangSwitcher />);
-
-  // const linkElement = screen.getByRole("button");
-  // expect(linkElement).toBeInTheDocument();
-  // console.log(getByRole("button"))
-});
+describe("<LangSwitcher />",() => {
+  test('renders LangSwitcher', () => {
+    render(<LangSwitcher />);
+    expect(screen).toBeDefined();
+  });
+})
