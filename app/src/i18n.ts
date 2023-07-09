6929eaa1df186ec4eb95875a30276c7145ed7e6a
kapepa <kapepayx@gmail.com>
2023-07-09 23:36:15 +0300

    app: feature/common | storybook

--- app/src/i18n.ts
+++ app/src/i18n.ts
@@ -4,15 +4,15 @@ import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-import enTranslation from "../public/locales/en/translation.json";
-import enHome from "../public/locales/en/home.json";
-import enAbout from "../public/locales/en/about.json";
-import enError from "../public/locales/en/error.json";
+import enTranslation from "./locales/en/translation.json";
+import enHome from "./locales/en/home.json";
+import enAbout from "./locales/en/about.json";
+import enError from "./locales/en/error.json";
 
-import ruTranslation from "../public/locales/ru/translation.json";
-import ruHome from "../public/locales/ru/home.json";
-import ruAbout from "../public/locales/ru/about.json";
-import ruError from "../public/locales/ru/error.json";
+import ruTranslation from "./locales/ru/translation.json";
+import ruHome from "./locales/ru/home.json";
+import ruAbout from "./locales/ru/about.json";
+import ruError from "./locales/ru/error.json";
 
 
 i18n
