fc603481a2e5d585e3b62e47f9d5b5dd8d18378c
kapepa <kapepayx@gmail.com>
2023-07-09 23:23:43 +0300

    app: feature/common | storybook

--- app/src/i18n.ts
+++ app/src/i18n.ts
@@ -4,6 +4,16 @@ import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
+import enTranslation from "../public/locales/en/translation.json";
+import enHome from "../public/locales/en/home.json";
+import enAbout from "../public/locales/en/about.json";
+import enError from "../public/locales/en/error.json";
+
+import ruTranslation from "../public/locales/ru/translation.json";
+import ruHome from "../public/locales/ru/home.json";
+import ruAbout from "../public/locales/ru/about.json";
+import ruError from "../public/locales/ru/error.json";
+
 
 i18n
   .use(Backend)
@@ -11,16 +21,32 @@ i18n
   .use(initReactI18next)
   // .use(resourcesToBackend((language, namespace) => import(`./locales/${language}/${namespace}.json`)))
   .init({
+    resources: {
+      en: {
+        translation: enTranslation,
+        home: enHome,
+        about: enAbout,
+        error: enError
+      },
+      ru: {
+        translation: ruTranslation,
+        home: ruHome,
+        about: ruAbout,
+        error: ruError
+      }
+    },
     fallbackLng: 'en',
     // debug: process.env.NODE_ENV === "development",
     debug: false,
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
-    // backend: {
-    //   // loadPath: 'http://localhost:3000/locales/{{lng}}/{{ns}}.json',
-    //   // loadPath: '../public/public/locales/{{lng}}/{{ns}}.json',
-    // },
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
+    },
+  }, (err, t) => {
+    if (err) return console.log('something went wrong loading', err);
+    t('key'); // -> same as i18next.t
   });
 
 
--- app/src/shared/config/i18nDecorator/i18nDecorator.tsx
+++ app/src/shared/config/i18nDecorator/i18nDecorator.tsx
@@ -1,4 +1,4 @@
-import { Suspense } from "react";
+import React, { Suspense } from "react";
 import { Decorator } from "@storybook/react";
 import { I18nextProvider } from "react-i18next";
 import i18n from  "../../../i18n";
