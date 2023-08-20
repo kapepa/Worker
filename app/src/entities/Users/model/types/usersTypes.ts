26a2f3584f87c1f5efc7ebea659663ca9d6f13c8
kapepa <kapepayx@gmail.com>
2023-08-20 23:08:48 +0300

    api: comments, articles unit-test

--- app/src/entities/Users/model/types/usersTypes.ts
+++ app/src/entities/Users/model/types/usersTypes.ts
@@ -1,4 +1,13 @@
+import {Countries} from "../../../../shared/const/Countries";
+
 export interface UsersTypes {
-  "id": string,
-  "username": string,
+  id: string,
+  username: string,
+  firstname?: string,
+  lastname?: string,
+  email?: string,
+  password?: string,
+  avatar?: string | File,
+  country?: Countries,
+  city?: string,
 }
