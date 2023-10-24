de8f602eb7c92ab5b0990fb6b8c3424e8554eb48
kapepa <kapepayx@gmail.com>
2023-10-24 21:00:48 +0300

    deploy

--- app/src/config.ts
+++ app/src/config.ts
@@ -1,5 +1,5 @@
 const config = {
-  api: process.env.NODE_ENV !== 'production' ? 'http://127.0.0.1:5000' : 'https://worker-7rafzentd-kapepa.vercel.app',
+  api: process.env.NODE_ENV !== 'production' ? 'https://worker-7rafzentd-kapepa.vercel.app' : 'https://worker-7rafzentd-kapepa.vercel.app',
 };
 
 export default config;
