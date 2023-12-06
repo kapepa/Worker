373aa3acf08aca502754a1835c71207e1972aa8e
kapepa <kapepayx@gmail.com>
2023-12-06 21:02:17 +0200

    app: widgets/PreLoaderAvatar

--- app/src/pages/Home/ui/Home.tsx
+++ app/src/pages/Home/ui/Home.tsx
@@ -4,10 +4,14 @@ import {Text, TextSize, TextTheme} from "../../../shared/ui/Text/Text";
 import {ListBox} from "../../../widgets/ListBox";
 import {ListBoxInt} from "../../../shared/interface/ListBoxInt";
 import {useCallback} from "react";
+import {PreLoaderAvatar} from "../../../widgets/PreLoaderAvatar";
+import {useSelector} from "react-redux";
+import {GetUsersProfile} from "../../../entities/Users";
 // import {RatingCard} from "../../../entities/Rating";
 
 function Home() {
   const { t } = useTranslation("home");
+  const profile = useSelector(GetUsersProfile);
   const people: ListBoxInt[] = [
     { name: 'Durward Reynolds', unavailable: false },
     { name: 'Kenton Towne', unavailable: false },
@@ -25,6 +29,13 @@ function Home() {
       <Text theme={TextTheme.PRIMARY} headerTag="h1" title={t("home page")} size={TextSize.XL}/>
       <ListBox list={people} defaultValue={people[0]} callback={changeListBox}/>
       {/*<RatingCard title={"Test Text"} callback={(data) => console.log(data)}/>*/}
+      <PreLoaderAvatar
+        url={profile?.avatar}
+        size={100}
+        alt={profile?.firstname}
+        api={true}
+        symbol={profile?.firstname && profile?.firstname.charAt(1)}
+      />
     </div>
   )
 }
