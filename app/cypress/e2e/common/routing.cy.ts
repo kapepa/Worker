e445a55502c65a93551e14ff12bf68e6ba9648f9
kapepa <kapepayx@gmail.com>
2023-12-10 23:38:04 +0200

    app cypress

--- app/cypress/e2e/common/routing.cy.ts
+++ app/cypress/e2e/common/routing.cy.ts
@@ -3,4 +3,14 @@ describe('Routing', () => {
     cy.visit('/');
     cy.get('[data-testid=home]').should('exist')
   })
+
+  it('About', () => {
+    cy.visit('/about');
+    cy.get('[data-testid=about]').should('exist')
+  })
+
+  it('Error', () => {
+    cy.visit('/not-found');
+    cy.get('[data-testid=error]').should('exist')
+  })
 })
